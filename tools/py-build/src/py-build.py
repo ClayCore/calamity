08ab7d7480d894f3c106fa721fea4b5aab068167
lunarium <claymorealt@op.pl>
2021-07-28 09:06:17 +0200

    Add launch args parsing using docopt

--- tools/py-build/src/py-build.py
+++ tools/py-build/src/py-build.py
@@ -1,9 +1,69 @@
 #!/usr/bin/env python3
 
+'''py-build.py -- simple build system
+
+Builds C/C++ projects based on YAML config.
+
+Usage:
+    py-build.py [--help]
+    py-build.py clean <config>
+    py-build.py build <config>
+    py-build.py run <config>
+
+Options:
+    --help              Shows this screen.
+
+Subcommands:
+    clean               Cleans build and output directories.
+    build               Builds the entire project.
+    run                 Builds and runs the application.
+'''
+
+import os
+import sys
+import colorama # type: ignore
 from utils.logger import log
+from docopt import docopt # type: ignore
 
 def main():
-    log.info('Initializing build system...')
+    # Clear the screen
+    if os.name == 'posix':
+        os.system('clear')
+    else:
+        os.system('cls')
+
+    # Initialize colorama for colored logging
+    colorama.init()
+
+    # Parsing launch parameters
+    # =========================
+
+    # Add `--help` if no arguments were supplied
+    if len(sys.argv) == 1:
+        sys.argv.append('--help')
+        args = docopt(__doc__, version=None, options_first=True)
+    else:
+        args = docopt(__doc__, version=None, options_first=True)
+
+        config_path = args['<config>']
+        if config_path:
+            log.debug('Found config path', path=config_path)
+
+        log.info('Initializing build system...')
+
+        if args['clean'] == True:
+            # Clean directories...
+            log.info('Starting clean-up...')
+            pass
+        if args['build'] == True:
+            # Build the project
+            log.info('Starting build...')
+            pass
+        if args['run'] == True:
+            # Build & run
+            log.info('Starting build & run...')
+            pass
+
 
 if __name__ == '__main__':
     main()
