83b37ec34c5cbda94ade17553880a57919ae4dee
lunarium <claymorealt@op.pl>
2021-07-30 11:14:58 +0200

    Add better logging library and color output

--- calamity/src/utils/log.h
+++ calamity/src/utils/log.h
@@ -1,18 +1,46 @@
+/**
+ * Copyright (c) 2020 rxi
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the MIT license. See `log.c` for details.
+ */
+
 #pragma once
 
-#include "common.h"
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <time.h>
 
-// different colors for printing log messages
-typedef enum { RESET = 0, RED = 31, GREEN, YELLOW, BLUE, MAGENTA, CYAN } log_colors;
+#define LOG_VERSION "0.1.0"
 
-// used internally for checking what log level we're printing on.
-enum log_levels_t { TRACE, INFO, DEBUG, WARN, ERROR };
-internal enum log_levels_t log_levels;
+typedef struct {
+    va_list     ap;
+    const char* fmt;
+    const char* file;
+    struct tm*  time;
+    void*       udata;
+    int         line;
+    int         level;
+} log_Event;
 
-void log_helper(enum log_levels_t level, i8 color, const char* format, ...);
+typedef void (*log_LogFn)(log_Event* ev);
+typedef void (*log_LockFn)(bool lock, void* udata);
 
-void log_trace(const char* format, ...);
-void log_info(const char* format, ...);
-void log_debug(const char* format, ...);
-void log_warn(const char* format, ...);
-void log_error(const char* format, ...);
+enum { LOG_TRACE, LOG_DEBUG, LOG_INFO, LOG_WARN, LOG_ERROR, LOG_FATAL };
+
+#define log_trace(...) log_log(LOG_TRACE, __FILE__, __LINE__, __VA_ARGS__)
+#define log_debug(...) log_log(LOG_DEBUG, __FILE__, __LINE__, __VA_ARGS__)
+#define log_info(...) log_log(LOG_INFO, __FILE__, __LINE__, __VA_ARGS__)
+#define log_warn(...) log_log(LOG_WARN, __FILE__, __LINE__, __VA_ARGS__)
+#define log_error(...) log_log(LOG_ERROR, __FILE__, __LINE__, __VA_ARGS__)
+#define log_fatal(...) log_log(LOG_FATAL, __FILE__, __LINE__, __VA_ARGS__)
+
+const char* log_level_string(int level);
+void        log_set_lock(log_LockFn fn, void* udata);
+void        log_set_level(int level);
+void        log_set_quiet(bool enable);
+int         log_add_callback(log_LogFn fn, void* udata, int level);
+int         log_add_fp(FILE* fp, int level);
+
+void log_log(int level, const char* file, int line, const char* fmt, ...);
