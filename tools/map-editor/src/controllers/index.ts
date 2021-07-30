f9ad0df2aacb42076e2a4e9a40b7f3a5f70332dc
lunarium <claymorealt@op.pl>
2021-07-30 09:00:34 +0200

    Fix controller routing of the wrong page

--- tools/map-editor/src/controllers/index.ts
+++ tools/map-editor/src/controllers/index.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from 'express';
 
 export const index = (req: Request, res: Response) => {
-    res.render('home', {
-        title: 'Home'
+    res.render('index', {
+        title: 'Home',
     });
 };
