535bc9d4379cad780e89e8bfd9454b0861d90d55
lunarium <claymorealt@op.pl>
2021-07-31 08:22:18 +0200

    Update file handling routines

--- calamity/src/utils/file.c
+++ calamity/src/utils/file.c
@@ -2,63 +2,69 @@
 #include "log.h"
 
 // Returns the filename without the path
-const char* F_Basename(const char* file_path) {
-    char* basename;
-    (basename = strrchr(file_path, '\\')) ? ++basename : (basename = file_path);
+const char* F_Basename(const char file_path[static 1]) {
+    const char* basename =
+        (basename = strrchr(file_path, '\\')) ? ++basename : (basename = file_path);
 
     return basename;
 }
 
 // Checks if any file exists.
-isize F_Exists(const char* file_path) {
+isize F_Exists(const char file_path[static 1]) {
     FILE* fd;
     if ((fd = fopen(file_path, "r")) != NULL) {
+        log_trace("F_Exists(): File \"%s\" exists", file_path);
         fclose(fd);
 
-        return 0;
+        return 1;
     } else {
-        log_error("Failed to open file.");
+        log_error("F_Exists(): Failed to open file.");
 
-        return -1;
+        return 0;
     }
 }
 
 // Loads the file and logs it.
 // TODO: Return the read file contents into a buffer
-isize F_LoadFile(const char* file_path) {
-    if (*file_path == '\0') {
-        log_error("F_LoadFile(): \"%s\" is empty or invalid!", file_path);
+usize F_LoadFile(const char file_path[static 1], char*** output_buffer) {
+    log_trace("F_LoadFile(): Opening \"%s\"", file_path);
 
-        return -1;
-    } else {
-        log_trace("F_LoadFile(): Opening \"%s\"", file_path);
-        i32 MAX_LEN = 512;
+    usize MAX_LEN   = 512;
+    usize MAX_LINES = 4096;
 
-        FILE* fd;
-        usize count = 0;
-        isize read  = 0;
+    FILE* fd;
+    usize count = 0;
+    usize read  = 0;
 
-        fd = fopen(file_path, "r");
-        if (!fd) {
-            log_error("Failed to open file.");
-            return read;
-        }
-
-        char  line[MAX_LEN];
-        usize len = 0;
-        while (fgets(line, sizeof(line), fd)) {
-            // Strip newlines
-            line[strcspn(line, "\n")] = 0;
-
-            len = strlen(line);
-            read += len;
-
-            log_trace("F_LoadFile(): [%d: \"%s\"]: %s", count, F_Basename(file_path), line);
-            count += 1;
-        }
-
-        fclose(fd);
-
+    // Check if the file is readable
+    fd = fopen(file_path, "r");
+    if (!fd) {
+        log_error("F_LoadFile(): Failed to open file.");
         return read;
     }
+
+    if (*output_buffer == NULL) {
+        *output_buffer = calloc(1, MAX_LINES);
+    }
+
+    char  line[MAX_LEN];
+    usize len = 0;
+    while (fgets(line, sizeof(line), fd)) {
+        // Strip newlines
+        line[strcspn(line, "\n")] = 0;
+
+        len = strlen(line);
+        read += len;
+
+        (*output_buffer)[count] = calloc(1, len);
+        strncpy((*output_buffer)[count], line, len);
+
+        count += 1;
+    }
+
+    log_trace("F_LoadFile(): Read %" PRIu32 " lines and %" PRIu32 " bytes", count, read);
+
+    fclose(fd);
+
+    return read;
 }
--- calamity/src/utils/file.h
+++ calamity/src/utils/file.h
@@ -2,6 +2,6 @@
 
 #include "common.h"
 
-const char* F_Basename(const char* file_path);
-isize       F_Exists(const char* file_path);
-isize       F_LoadFile(const char* file_path);
+const char* F_Basename(const char file_path[static 1]);
+isize       F_Exists(const char file_path[static 1]);
+usize       F_LoadFile(const char file_path[static 1], char*** output_buffer);
