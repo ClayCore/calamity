6fcda07def47096e2cbac639672caef2373b9322
lunarium <claymorealt@op.pl>
2021-08-02 16:14:30 +0200

    Fix lambda captures

--- calamity/src/yaml/lookup.hpp
+++ calamity/src/yaml/lookup.hpp
@@ -12,10 +12,10 @@ namespace YAML::LUTs {
 
     // Used for parsing different types of level objects from map files
     static std::map<std::string, Functor> PARSER_FUNCTORS = {
-        { "PARSE_ENTS", [&](const FuncArgs& args) { return parse_functor<Level::Entity>(args); } },
-        { "PARSE_VERTS", [&](const FuncArgs& args) { return parse_functor<Level::Vec2>(args); } },
-        { "PARSE_LINES", [&](const FuncArgs& args) { return parse_functor<Level::Line>(args); } },
-        { "PARSE_SIDES", [&](const FuncArgs& args) { return parse_functor<Level::Side>(args); } },
-        { "PARSE_SECTORS", [&](const FuncArgs& args) { return parse_functor<Level::Sector>(args); } },
+        { "PARSE_ENTS", [](const FuncArgs& args) { return parse_functor<Level::Entity>(args); } },
+        { "PARSE_VERTS", [](const FuncArgs& args) { return parse_functor<Level::Vec2>(args); } },
+        { "PARSE_LINES", [](const FuncArgs& args) { return parse_functor<Level::Line>(args); } },
+        { "PARSE_SIDES", [](const FuncArgs& args) { return parse_functor<Level::Side>(args); } },
+        { "PARSE_SECTORS", [](const FuncArgs& args) { return parse_functor<Level::Sector>(args); } },
     };
 } // namespace YAML::LUTs
