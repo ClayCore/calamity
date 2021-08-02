43db64ca1c79678d1da9603d4eaf21e2e3710fb2
lunarium <claymorealt@op.pl>
2021-08-02 20:55:54 +0200

    Rename `processed` to `functor`

--- calamity/src/yaml/parser.cpp
+++ calamity/src/yaml/parser.cpp
@@ -26,38 +26,38 @@ namespace YAML {
                 // Sadly, we are forced to cast these constantly...
                 // Blame me for using std::any for this
                 // couldn't figure out how to use std::variant with it
-                auto    raw       = LUTs::PARSER_FUNCTORS["PARSE_ENTS"];
-                Functor processed = std::any_cast<Functor>(raw);
+                auto    raw     = LUTs::PARSER_FUNCTORS["PARSE_ENTS"];
+                Functor functor = std::any_cast<Functor>(raw);
 
                 // acquire the result
-                std::vector<Entity> result = processed(Types::FuncArgs{ data, index });
+                std::vector<Entity> result = functor(Types::FuncArgs{ data, index });
             } else if (current_line.rfind("verts", 0) == 0) {
                 using Functor = std::function<std::vector<Vec2>(const Types::FuncArgs& args)>;
 
                 std::cout << "VERTICES" << std::endl;
 
-                auto    raw       = LUTs::PARSER_FUNCTORS["PARSE_VERTS"];
-                Functor processed = std::any_cast<Functor>(raw);
+                auto    raw     = LUTs::PARSER_FUNCTORS["PARSE_VERTS"];
+                Functor functor = std::any_cast<Functor>(raw);
 
-                std::vector<Vec2> result = processed(Types::FuncArgs{ data, index });
+                std::vector<Vec2> result = functor(Types::FuncArgs{ data, index });
             } else if (current_line.rfind("lines", 0) == 0) {
                 using Functor = std::function<std::vector<Line>(const Types::FuncArgs& args)>;
 
                 std::cout << "LINES" << std::endl;
 
-                auto    raw       = LUTs::PARSER_FUNCTORS["PARSE_LINES"];
-                Functor processed = std::any_cast<Functor>(raw);
+                auto    raw     = LUTs::PARSER_FUNCTORS["PARSE_LINES"];
+                Functor functor = std::any_cast<Functor>(raw);
 
-                std::vector<Line> result = processed(Types::FuncArgs{ data, index });
+                std::vector<Line> result = functor(Types::FuncArgs{ data, index });
             } else if (current_line.rfind("sides", 0) == 0) {
                 using Functor = std::function<std::vector<Side>(const Types::FuncArgs& args)>;
 
                 std::cout << "SIDES" << std::endl;
 
-                auto    raw       = LUTs::PARSER_FUNCTORS["PARSE_SIDES"];
-                Functor processed = std::any_cast<Functor>(raw);
+                auto    raw     = LUTs::PARSER_FUNCTORS["PARSE_SIDES"];
+                Functor functor = std::any_cast<Functor>(raw);
 
-                std::vector<Side> result = processed(Types::FuncArgs{ data, index });
+                std::vector<Side> result = functor(Types::FuncArgs{ data, index });
             } else if (current_line.rfind("sectors", 0) == 0) {
                 using Functor = std::function<std::vector<Sector>(const Types::FuncArgs& args)>;
 
