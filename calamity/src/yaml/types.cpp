5929bfb06ee4713cea9d9b3d172d7f0f1ab55354
lunarium <claymorealt@op.pl>
2021-08-02 20:18:14 +0200

    Add saving parsed `Level::Entity` to vector

--- calamity/src/yaml/types.cpp
+++ calamity/src/yaml/types.cpp
@@ -9,10 +9,12 @@ namespace YAML::Types {
 
     template <>
     auto parse_functor<Level::Entity>(const FuncArgs& args) -> void {
+        Level::Entity ent     = {};
+        Level::Vec2   ent_pos = {};
+
+        std::vector<Level::Entity> entities;
         for (auto it = args.data.begin() + args.index; it < args.data.end(); ++it) {
-            // Get the current index (which line) from the iterator
-            usize       cur_idx = it - args.data.begin();
-            std::string line    = *it;
+            std::string line = *it;
 
             // Break when we hit newlines or empty strings
             // these signify the end of our parsing block
@@ -23,18 +25,32 @@ namespace YAML::Types {
             // If no "end of stream" markers have been found
             // parse the following lines as dictionaries in arrays.
             if (line.rfind('-', 0)) {
-                std::string value = line;
-
                 // Identify where in the string the value starts and key ends
-                auto pos = value.find(':', 0);
+                auto pos = line.find(':', 0);
 
-                // Erase it
-                value.erase(0, pos + 1);
+                // Split the string into a key-value pair
+                std::string key   = line.substr(0, pos);
+                std::string value = line.substr(pos + 1);
 
-                std::cout << "\tParsed: [" << value << "]" << std::endl;
+                // No switch statements for non-integral types...
+                if (key == "x") {
+                    ent_pos.x = std::stod(value);
+                } else if (key == "y") {
+                    ent_pos.y = std::stod(value);
+                } else if (key == "angle") {
+                    ent.angle = static_cast<u16>(std::stod(value));
+                } else if (key == "type") {
+                    ent.type = static_cast<u16>(std::stod(value));
+                }
+
+                std::cout << "\tParsed: [" << key << "]:[" << value << "]" << std::endl;
             } else {
                 continue;
             }
+
+            // Save the entity into a vector
+            ent.pos = ent_pos;
+            entities.push_back(ent);
         }
         return;
     }
