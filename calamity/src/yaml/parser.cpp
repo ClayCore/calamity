44145eaaa718ad03cb16b8f86b4d992e50b29312
lunarium <claymorealt@op.pl>
2021-08-04 11:09:46 +0200

    Remove functors, tables and isolated functions

--- calamity/src/yaml/parser.cpp
+++ calamity/src/yaml/parser.cpp
@@ -12,62 +12,13 @@ namespace YAML {
         }
 
         for (auto it = data.begin(); it < data.end(); ++it) {
-            std::string current_line = *it;
+            std::string str = *it;
 
-            // Index of the current line in the vector.
-            // prevents parsing backwards
-            usize index = (it - data.begin()) + 1;
+            // Offset from the start of the vec
+            // Prevents parsing of elements backwards
+            usize offset = (it - data.begin()) + 1;
 
-            if (current_line.rfind("ents", 0) == 0) {
-                using Functor = LUTs::LambdaEnt;
-
-                std::cout << "ENTITIES" << std::endl;
-
-                // Sadly, we are forced to cast these constantly...
-                // Blame me for using std::any for this
-                // couldn't figure out how to use std::variant with it
-                auto    raw     = LUTs::PARSER_FUNCTORS["PARSE_ENTS"];
-                Functor functor = std::any_cast<Functor>(raw);
-
-                // acquire the result
-                std::vector<Entity> result = functor(Types::FuncArgs{ data, index });
-            } else if (current_line.rfind("verts", 0) == 0) {
-                using Functor = LUTs::LambdaVert;
-
-                std::cout << "VERTICES" << std::endl;
-
-                auto    raw     = LUTs::PARSER_FUNCTORS["PARSE_VERTS"];
-                Functor functor = std::any_cast<Functor>(raw);
-
-                std::vector<Vec2> result = functor(Types::FuncArgs{ data, index });
-            } else if (current_line.rfind("lines", 0) == 0) {
-                using Functor = LUTs::LambdaLine;
-
-                std::cout << "LINES" << std::endl;
-
-                auto    raw     = LUTs::PARSER_FUNCTORS["PARSE_LINES"];
-                Functor functor = std::any_cast<Functor>(raw);
-
-                std::vector<Line> result = functor(Types::FuncArgs{ data, index });
-            } else if (current_line.rfind("sides", 0) == 0) {
-                using Functor = LUTs::LambdaSide;
-
-                std::cout << "SIDES" << std::endl;
-
-                auto    raw     = LUTs::PARSER_FUNCTORS["PARSE_SIDES"];
-                Functor functor = std::any_cast<Functor>(raw);
-
-                std::vector<Side> result = functor(Types::FuncArgs{ data, index });
-            } else if (current_line.rfind("sectors", 0) == 0) {
-                using Functor = LUTs::LambdaSector;
-
-                std::cout << "SECTORS" << std::endl;
-
-                auto    raw       = LUTs::PARSER_FUNCTORS["PARSE_SECTORS"];
-                Functor processed = std::any_cast<Functor>(raw);
-
-                std::vector<Sector> result = processed(Types::FuncArgs{ data, index });
-            }
+            auto level = Types::Level::parse_level({ data, offset });
         }
     }
 } // namespace YAML
