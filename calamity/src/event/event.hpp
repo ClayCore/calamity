f2fa4230572e8181857024a09a6c9359bcdad681
lunarium <claymorealt@op.pl>
2021-08-06 21:50:23 +0200

    Add new event module constructs

--- calamity/src/event/event.hpp
+++ calamity/src/event/event.hpp
@@ -7,18 +7,26 @@ namespace EventSystem
     enum class EventType : u16 {
         None = 0,
         // Window handling events
+        // ======================
         WindowClose,
         WindowResize,
         WindowFocus,
         WindowLostFocus,
         WindowMoved,
 
+        // Resource handling
+        // =================
+        LoadResource,
+        FreeResource,
+
         // Engine internals related
+        // ========================
         EngineTick,
         EngineUpdate,
         EngineRender,
 
         // Input handling
+        // ==============
         KeyPressed,
         KeyReleased,
         MouseButtonPressed,
@@ -27,38 +35,23 @@ namespace EventSystem
         MouseScrolled
     };
 
-    class Event
+    // Base event class
+    // Derive for specialized events
+    // ==================================
+    class BaseEvent
     {
-      public:
-        bool handled = false;
+        public:
+        // TODO: Test whether `this->m_type` has to be
+        // a ref or a const ref or just a value.
+        BaseEvent(const EventType& type) : m_type(type) {}
+
+        // Accessor methods
+        // ================
+        virtual const char*
+        get_name() const = 0;
 
         virtual EventType
-        get_event_type() const = 0;
-
-        virtual const char*
-        get_name() const
-        {
-            switch (this->get_event_type()) {
-                    // clang-format off
-                
-                case EventType::None                    : return "None";
-                case EventType::WindowClose             : return "WindowClose";
-                case EventType::WindowResize            : return "WindowResize";
-                case EventType::WindowFocus             : return "WindowFocus";
-                case EventType::WindowLostFocus         : return "WindowLostFocus";
-                case EventType::WindowMoved             : return "WindowMoved";
-                case EventType::EngineTick              : return "EngineTick";
-                case EventType::EngineUpdate            : return "EngineUpdate";
-                case EventType::KeyPressed              : return "KeyPressed";
-                case EventType::KeyReleased             : return "KeyReleased";
-                case EventType::MouseButtonPressed      : return "MouseButtonPressed";
-                case EventType::MouseButtonReleased     : return "MouseButtonReleased";
-                case EventType::MouseMoved              : return "MouseMoved";
-                case EventType::MouseScrolled           : return "MouseScrolled";
-
-                    // clang-format on
-            }
-        }
+        get_type() const = 0;
 
         virtual std::string
         to_string() const
@@ -66,13 +59,121 @@ namespace EventSystem
             return this->get_name();
         }
 
+        // Overloads
+        // =========
         friend inline std::ostream&
-        operator<<(std::ostream& os, const Event& e)
+        operator<<(std::ostream& os, const BaseEvent& e)
         {
             return os << e.to_string();
         }
 
-        // TODO: Create an event dispatcher class
-        // ======================================
+        private:
+        // Type of the event
+        // =================
+        const EventType& m_type;
     };
+
+    // Base dispatcher
+    // Derive to send and release distinct events
+    class BaseDispatcher
+    {
+    };
+
+    // Base listener
+    // Derive to listen to specific events
+    // ===================================
+    class BaseListener
+    {
+        private:
+        using EvPtr    = std::unique_ptr<BaseEvent>;
+        using Dispatch = std::unique_ptr<BaseDispatcher>;
+        using Callback = std::function<void()>;
+
+        public:
+        // Handles a distinct event
+        // sent by a released dispatch
+        virtual void
+        on_event(const EvPtr& event);
+
+        private:
+        Dispatch m_dispatcher;
+    };
+
+    // Base emitter
+    // Derive to be able to emit
+    // specific events, to be captured by distinct dispatchers
+    // and listeners.
+    // =======================================================
+    class BaseEmitter
+    {
+        private:
+        // Handy typedef
+        using EvPtr   = std::shared_ptr<BaseEvent>;
+        using DispPtr = std::unique_ptr<BaseDispatcher>;
+
+        public:
+        // Accessor methods
+        // ================
+        virtual void
+        emit();
+
+        virtual DispPtr
+        get_dispatcher()
+        {
+            return std::move(this->m_dispatcher);
+        }
+
+        virtual std::string
+        to_string() const
+        {
+            std::vector<std::string> buffer;
+            for (auto it = this->m_events.begin(); it < this->m_events.end(); ++it) {
+                // Extract the event from the array
+                EvPtr event = *it;
+
+                // Acquire the string from the event
+                std::string event_str = event->to_string().c_str();
+
+                // Get the index of the current iteration
+                // from the iterator
+                usize index = static_cast<usize>(it - this->m_events.begin());
+
+                // Start formatting into the string
+                auto        format = "%u: [%s]\n";
+                auto        size   = std::snprintf(nullptr, 0, format);
+                std::string output(size + 1, '\0');
+                std::sprintf(&output[0], format, index, event_str);
+
+                // Add it to the vector
+                buffer.push_back(output);
+            }
+
+            // Implode the buffer into a single string
+            std::ostringstream implode;
+            std::copy(buffer.begin(), buffer.end(), std::ostream_iterator<std::string>(implode, "\n"));
+
+            return implode.str();
+        }
+
+        // Overloads
+        // =========
+        friend inline std::ostream&
+        operator<<(std::ostream& os, const BaseEmitter& be)
+        {
+            return os << be.to_string();
+        }
+
+        private:
+        // Set of events to be released by this emitter
+        // TODO: maybe use a vec or a map?
+        // ============================================
+        std::vector<EvPtr> m_events;
+
+        // The dispatcher this emitter connects to
+        // TODO: there could be multiple dispatchers
+        // for a single event. Implement this later.
+        // =========================================
+        DispPtr m_dispatcher;
+    };
+
 } // namespace EventSystem
