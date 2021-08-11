0334b567284cd90cadc4729527eec72c04d79aad
lunarium <claymorealt@op.pl>
2021-08-11 19:23:15 +0200

    Fix update method in graphics manager

--- calamity/src/graphics/manager.cpp
+++ calamity/src/graphics/manager.cpp
@@ -11,9 +11,6 @@ namespace GFX
         window.create_window();
         window.init_glad();
 
-        bool running = true;
-        while (running && !glfwWindowShouldClose(window.m_window.get())) {
-            running = window.on_update();
-        }
+        window.on_update();
     }
 } // namespace GFX
