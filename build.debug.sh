9f0a9c2af3d94188da9b2a3a3e77b4bf91775900
lunarium <claymorealt@op.pl>
2021-08-16 15:04:25 +0200

    Add support for multi-platform build

--- build.debug.sh
+++ build.debug.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-pushd tools/py-build > /dev/null
+pushd tools/py-build > /dev/null || exit
 
-poetry run python src/py-build.py build ../../calamity/build.yaml debug
+if [[ "$OSTYPE" =~ ^linux-gnu* ]]; then
+    poetry run python src/py-build.py build ../../calamity/build.linux.yaml debug
+elif [[ "$OSTYPE" =~ ^msys* ]] || [[ "$OSTYPE" =~ ^cygwin* ]]; then
+    poetry run python src/py-build.py build ../../calamity/build.win32.yaml debug
+else
+    echo "Unrecognized operating system"
+fi
 
-popd > /dev/null
+popd > /dev/null || exit
--- build.release.sh
+++ build.release.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-pushd tools/py-build > /dev/null
+pushd tools/py-build > /dev/null || exit
 
-poetry run python src/py-build.py build ../../calamity/build.yaml release
+if [[ "$OSTYPE" =~ ^linux-gnu* ]]; then
+    poetry run python src/py-build.py build ../../calamity/build.linux.yaml release
+elif [[ "$OSTYPE" =~ ^msys* ]] || [[ "$OSTYPE" =~ ^cygwin* ]]; then
+    poetry run python src/py-build.py build ../../calamity/build.win32.yaml release
+else
+    echo "Unrecognized operating system"
+fi
 
-popd > /dev/null
+popd > /dev/null || exit
