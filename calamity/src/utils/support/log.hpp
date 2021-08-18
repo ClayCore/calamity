19343c85ea57a6f4f3d2bb3e3092fabf5df6843c
lunarium <claymorealt@op.pl>
2021-08-18 15:25:47 +0200

    Add basic logging submodule stub

---
+++ calamity/src/utils/support/log.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "zcommon.hpp"
+
+namespace Calamity::Log
+{
+    enum class Level { Trace, Debug, Info, Warn, Error, Critical };
+
+    class Sink
+    {
+        public:
+        virtual ~Sink() = default;
+
+        virtual void
+        log(Level level, const std::string& msg) = 0;
+    };
+
+    class StdoutSink final : public Sink
+    {
+        explicit StdoutSink();
+        ~StdoutSink();
+
+        void
+        log(Level level, const std::string& msg) override;
+
+        std::mutex m_lock;
+    };
+
+    class Logger
+    {
+    };
+} // namespace Calamity::Log
